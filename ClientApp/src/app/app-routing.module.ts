8fd9e2b21481f692a788781f3aee5ebe469a473e
josetoro19 <jftc1994@gmail.com>
2019-11-01 00:27:30 -0500

    ok

---
+++ ClientApp/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskAddComponent } from './task-add/task-add.component';
+import { TaskEditComponent } from './task-edit/task-edit.component';
+const routes: Routes = [
+{
+path:'tasklist',
+component:TaskListComponent
+},
+{
+path:'taskadd',
+component:TaskAddComponent
+},
+{
+path:'taskedit/:id',
+component:TaskEditComponent
+}
+];
+@NgModule({
+imports: [RouterModule.forRoot(routes)],
+exports: [RouterModule]
+})
+export class AppRoutingModule { }
